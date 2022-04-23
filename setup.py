48ab8643cf66302edb6ee0bbffb5c78c652bfa42
林柏維 <r05521501@ntu.edu.tw>
2022-04-23 15:41:29 -0400

    0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="web3-constant",
-    version="0.0.2",
+    version="0.0.3",
     author="Powei Lin",
     author_email="poweilin1994@gmail.com",
     description="Useful web3 constant",
